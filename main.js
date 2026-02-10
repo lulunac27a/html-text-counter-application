845c3c29824a4a8205d5a7b7ea55c8d163dd5480
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-02-10 10:46:19 -0600

    Add Prettier and format code

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const textBoxText = document.getElementById("text-box");
 const calculateButton = document.getElementById("calculate");
 const outputDiv = document.getElementById("output");
 calculateButton.addEventListener("click", () => {
-  const textContent = textBoxText.value;
-  const characters = textContent.length;
-  const words =
-    textContent.trim() === "" ? 0 : textContent.trim().split(/\s+/).length;
-  const lines = textContent.split("\n").length;
-  outputDiv.innerText = `Characters: ${characters}\nWords: ${words}\nLines: ${lines}`;
+    const textContent = textBoxText.value;
+    const characters = textContent.length;
+    const words =
+        textContent.trim() === "" ? 0 : textContent.trim().split(/\s+/).length;
+    const lines = textContent.split("\n").length;
+    outputDiv.innerText = `Characters: ${characters}\nWords: ${words}\nLines: ${lines}`;
 });
