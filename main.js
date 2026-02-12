82934756cd27ff04fb1f0361244a880b057952ce
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-02-12 10:52:16 -0600

    Change keyword name

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const textBoxText = document.getElementById("text-box");
 const calculateButton = document.getElementById("calculate");
-const outputDiv = document.getElementById("output");
+const outputText = document.getElementById("output");
 calculateButton.addEventListener("click", () => {
     const textContent = textBoxText.value;
     const characters = textContent.length;
     const words =
         textContent.trim() === "" ? 0 : textContent.trim().split(/\s+/).length;
     const lines = textContent.split("\n").length;
-    outputDiv.innerText = `Characters: ${characters}\nWords: ${words}\nLines: ${lines}`;
+    outputText.innerText = `Characters: ${characters}\nWords: ${words}\nLines: ${lines}`;
 });
