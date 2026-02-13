b697ad9ada3dcfd8097b6bdf9a5e4abb266a2110
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-02-12 18:32:27 -0600

    Add comments in JavaScript

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const textBoxText = document.getElementById("text-box");
-const calculateButton = document.getElementById("calculate");
-const outputText = document.getElementById("output");
+const textBoxText = document.getElementById("text-box"); //text box input
+const calculateButton = document.getElementById("calculate"); //calculate button
+const outputText = document.getElementById("output"); //output text area
 calculateButton.addEventListener("click", () => {
-    const textContent = textBoxText.value;
-    const characters = textContent.length;
+    //when calculate button is pressed
+    const textContent = textBoxText.value; //get text box input value
+    const characters = textContent.length; //calculate number of characters
     const words =
-        textContent.trim() === "" ? 0 : textContent.trim().split(/\s+/).length;
-    const lines = textContent.split("\n").length;
-    outputText.innerText = `Characters: ${characters}\nWords: ${words}\nLines: ${lines}`;
+        textContent.trim() === "" ? 0 : textContent.trim().split(/\s+/).length; //calculate number of words
+    const lines = textContent.split("\n").length; //calculate number of lines
+    outputText.innerText = `Characters: ${characters}\nWords: ${words}\nLines: ${lines}`; //update output text area
 });
